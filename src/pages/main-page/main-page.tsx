ded61cbaa091752e19e7976ffe9e1c691eabd68e
Mit'ka Kuznetsov <www.kuznetsovsky@gmail.com>
2022-09-25 23:35:48 +0300

    📦 Изменяет пути к статическим файлам на абсолютные

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -7,7 +7,7 @@ export default function MainPage(): JSX.Element {
           <div className="header__wrapper">
             <div className="header__left">
               <a className="header__logo-link header__logo-link--active">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
+                <img className="header__logo" src="/img/logo.svg" alt="6 cities logo" width="81" height="41" />
               </a>
             </div>
             <nav className="header__nav">
@@ -96,7 +96,7 @@ export default function MainPage(): JSX.Element {
                   </div>
                   <div className="cities__image-wrapper place-card__image-wrapper">
                     <a href="#">
-                      <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place image" />
+                      <img className="place-card__image" src="/img/apartment-01.jpg" width="260" height="200" alt="Place image" />
                     </a>
                   </div>
                   <div className="place-card__info">
@@ -128,7 +128,7 @@ export default function MainPage(): JSX.Element {
                 <article className="cities__place-card place-card">
                   <div className="cities__image-wrapper place-card__image-wrapper">
                     <a href="#">
-                      <img className="place-card__image" src="img/room.jpg" width="260" height="200" alt="Place image"/>
+                      <img className="place-card__image" src="/img/room.jpg" width="260" height="200" alt="Place image"/>
                     </a>
                   </div>
                   <div className="place-card__info">
@@ -160,7 +160,7 @@ export default function MainPage(): JSX.Element {
                 <article className="cities__place-card place-card">
                   <div className="cities__image-wrapper place-card__image-wrapper">
                     <a href="#">
-                      <img className="place-card__image" src="img/apartment-02.jpg" width="260" height="200" alt="Place image" />
+                      <img className="place-card__image" src="/img/apartment-02.jpg" width="260" height="200" alt="Place image" />
                     </a>
                   </div>
                   <div className="place-card__info">
@@ -195,7 +195,7 @@ export default function MainPage(): JSX.Element {
                   </div>
                   <div className="cities__image-wrapper place-card__image-wrapper">
                     <a href="#">
-                      <img className="place-card__image" src="img/apartment-03.jpg" width="260" height="200" alt="Place image"/>
+                      <img className="place-card__image" src="/img/apartment-03.jpg" width="260" height="200" alt="Place image"/>
                     </a>
                   </div>
                   <div className="place-card__info">
@@ -227,7 +227,7 @@ export default function MainPage(): JSX.Element {
                 <article className="cities__place-card place-card">
                   <div className="cities__image-wrapper place-card__image-wrapper">
                     <a href="#">
-                      <img className="place-card__image" src="img/room.jpg" width="260" height="200" alt="Place image" />
+                      <img className="place-card__image" src="/img/room.jpg" width="260" height="200" alt="Place image" />
                     </a>
                   </div>
                   <div className="place-card__info">
